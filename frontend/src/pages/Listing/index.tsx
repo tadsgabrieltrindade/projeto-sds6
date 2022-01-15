a6770bcd25b355406d62f8d3263c0993664e94c5
Gabriel Trindade <icandoitgrt@gmail.com>
2022-01-15 13:49:06 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,18 @@
+import axios from 'axios';
+
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from 'Utils/requests';
 
 function Listing() {
+
+  //Forma Errada
+  axios.get(`${BASE_URL}/movies?size=3`)
+  .then(response => {
+    console.log(response.data);
+  })
+
+
   return (
     <>
       <Pagination />
