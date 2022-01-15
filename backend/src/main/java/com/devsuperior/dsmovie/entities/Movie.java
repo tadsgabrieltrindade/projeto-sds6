af0bf5fa83f59544008f647f7378fe63c9b80950
Gabriel Trindade <icandoitgrt@gmail.com>
2022-01-15 09:45:43 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 @Table(name = "tb_movie")
@@ -20,6 +23,9 @@ public class Movie implements Serializable {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 
 	public Movie() {
 	}
@@ -72,4 +78,7 @@ public class Movie implements Serializable {
 		this.image = image;
 	}
 
+	public Set<Score> getScores() {
+		return scores;
+	}  
 }
