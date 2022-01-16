f74f5df37f60200ea2e35228b39e59ed89a9dc56
Gabriel Trindade <icandoitgrt@gmail.com>
2022-01-16 13:32:11 -0300

    customização

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <section className='texto'>
+        <p>Desenvolvido por Gabriel Trindade </p>
+        <p> Semana Spring React - <a href="https://github.com/devsuperior/sds-dsmovie" target='_blank' className="item_2">DevSuperior</a></p>
+        <p className='logo'>Projeto DSMovie 2022</p>
+      </section>
+
+      <section id="social">
+
+        <a href="http://github.com/tadsgabrieltrindade" target="__new" className='item'> 
+            <FaGithub />
+        </a>
+
+        <a href="https://www.linkedin.com/in/tadsgabrieltrindade" target="__new" className='item'> 
+            <FaLinkedin />
+        </a>
+
+        <a href="https://instagram.com/tadsgabrieltrindade" target="__new" className='item'> 
+            <FaInstagram />
+        </a>
+        
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
