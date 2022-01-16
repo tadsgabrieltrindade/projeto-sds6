128e347d97a4af4dc32dbccf55454b8e3e813595
Gabriel Trindade <icandoitgrt@gmail.com>
2022-01-15 21:16:36 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,14 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { BASE_URL } from 'Utils/requests';
-import { MoviePage } from 'types/Movie';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BASE_URL } from "Utils/requests";
+import { MoviePage } from "types/Movie";
 
 function Listing() {
-
   //paginação
   const [pageNumber, setPageNumber] = useState(0);
-  
+
   //este estado guarda a página que foi buscado na requisição
   const [page, setPage] = useState<MoviePage>({
     content: [],
@@ -20,26 +19,34 @@ function Listing() {
     number: 0,
     first: true,
     numberOfElements: 0,
-    empty: true
-});
-
+    empty: true,
+  });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
-    .then(response =>{
-      const data = response.data as MoviePage;
-      setPage(data);
-    })
-  }, [pageNumber])
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
 
-
-  
+  //função responsável por mudar o número da página
+  const handlePageChange = (newPageNumber : number) =>{
+    setPageNumber(newPageNumber);
+  }
 
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange}/>
       <div className="container">
         <div className="row">
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
+
           {/* Poderia ser também:
           {page.content.map(movie =>{
                 return (
@@ -48,18 +55,6 @@ function Listing() {
                 </div>
                 )
             })} */}
-
-            {page.content.map(movie => (
-                  <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard movie={movie} />
-                </div>
-                )
-            )}
-          
-       
-
-         
-          
         </div>
       </div>
     </>
